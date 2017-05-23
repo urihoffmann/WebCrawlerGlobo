d814d5544ea64368c8aef54404879ff794a14cec
Urique Hoffmann <urique.hoffmann@neemu.com>
2017-05-23 17:00:38 -0400

    Finishing V0

--- crawler.py
+++ crawler.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-
+import simplejson as json
 class Crawler:
 
     def getContent(self, url):
@@ -8,11 +8,64 @@ class Crawler:
         soup = BeautifulSoup(content, "lxml-xml")
         return soup
 
+    def parseDescritionP(self, p):
+        p = BeautifulSoup(p.get_text(), "lxml")
+        if len(p.html.body.p.get_text()) > 1:
+            return {
+                'type': 'text',
+                'content': p.html.body.p.get_text()
+            }
+        else:
+            return None
+
+    def parseDescritionDivImg(self, div):
+        return {
+            'type': 'image',
+            'content': div.img['src']
+        }
+
+
+    def parseDescritionDivUl(self, div):
+        content = []
+        for li in div.ul.findAll('li'):
+            content.append(li.a['href'])
+        return {
+            'type': 'links',
+            'content': content
+        }
+
+    def parseDescritionDiv(self, div):
+        if div.find('img'):
+            return self.parseDescritionDivImg(div)
+        elif div.find('ul'):
+            return self.parseDescritionDivUl(div)
+        else:
+            return None
+
     def parseContentDescription(self, description):
-        for p in description.findAll('p'):
-            print p
+        feedDescription = []
+        description = BeautifulSoup(description, "lxml")
+
+        for child in description.descendants:
+            if child.name == 'p':
+                feedP = self.parseDescritionP(child)
+                if feedP:
+                    feedDescription.append(feedP)
+            elif child.name == 'div':
+                feedDiv = self.parseDescritionDiv(child)
+                if feedDiv:
+                    feedDescription.append(feedDiv)
+        return feedDescription
+
 
     def parseContent(self, content):
+        feed = {
+            'feed' : [],
+        }
         for item in content.findAll('item'):
-            print item.title.get_text()
-            # return
+            feedItem = {}
+            feedItem['title'] = item.title.get_text()
+            feedItem['link'] = item.link.get_text()
+            feedItem['description'] = self.parseContentDescription(item.description.get_text())
+            feed['feed'].append(feedItem)
+        return json.dumps(feed)
--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from crawler import Crawler
+# from bs4 import BeautifulSoup
 
 def main():
     crawler = Crawler()
     content = crawler.getContent("http://revistaautoesporte.globo.com/rss/ultimas/feed.xml")
-    crawler.parseContent(content)
+    # content = open('test.xml').read()
+    # content = BeautifulSoup(content, "lxml-xml")
+    parsedContent = crawler.parseContent(content)
+    f = open('feed.json', 'w')
+    f.write(parsedContent)
 
 if __name__ == '__main__':
     main()
